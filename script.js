41241e2898bedbb6a990a8f7833834362875889a
Henrique <99288725+Henrique11154@users.noreply.github.com>
2023-10-25 10:31:44 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 let select = document.querySelector("#select")
 let nomes = "https://pokemon.danielpimentel.com.br/v1/pokemon/lista"
-
-function mostrarMonstro(url){
+let div = document.querySelector(".div")
+function pegarPokemon(url){
     for(let i = 0; i <= 904; i++){
         fetch(url).then(resposta => resposta.json())
-        .then(pokemon => select.innerHTML += `<option>${pokemon.pokemon[i].nome}</option>`)//select.innerHTML += `<option>${pokemon.pokemon[i].nome}</option>`)
+        .then(pokemon => select.innerHTML += `<option value="${pokemon.pokemon[i].nome}" class="abajur">${pokemon.pokemon[i].nome}</option>`)//select.innerHTML += `<option>${pokemon.pokemon[i].nome}</option>`)
         .catch(erro => console.error(erro))
     }
 }
-mostrarMonstro(nomes)
+pegarPokemon(nomes)
+
+function mostrarPokemon(){
+    nameValue = ab.value
+    console.log(nameValue)
+}
